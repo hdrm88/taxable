5027d6a23f339aba98c9f0eb88124f47670fe035
heydar878 <h.manssourian@campus.tu-berlin.de>
2023-02-05 23:32:49 +0100

    first commit

---
+++ taxable/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import HomePage from "./components/HomePage.vue";
+import HomeRight from "./components/HomeRight.vue";
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.png"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HomePage msg="taxable" />
+      
+    </div>
+  </header>
+
+  <main>
+   <div>
+    <HomeRight />
+   </div>
+ </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ taxable/src/components/HomeRight.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+
+});
+</script>
+
+<template>
+  <div>
+    <label for="Brutto">Brutto: </label>
+    
+    <input type="number" >
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
